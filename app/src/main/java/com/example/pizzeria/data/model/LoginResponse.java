84033358ea1e899ed86c9c5ae0a6ecb11b361d15
Weura <weramaj1708@gmail.com>
2025-01-07 21:08:33 +0100

    logowanie done

--- app/src/main/java/com/example/pizzeria/data/model/LoginResponse.java
+++ app/src/main/java/com/example/pizzeria/data/model/LoginResponse.java
@@ -1,51 +1,23 @@
 package com.example.pizzeria.data.model;
 
 public class LoginResponse {
+    private String message;
+    private int userId;
 
-    private String userId;
-    private String userName;
-    private String token; // Jeśli API zwraca token
-
-    // Konstruktor (jeśli będziesz inicjalizować obiekt z wartościami)
-    public LoginResponse(String userId, String userName, String token) {
-        this.userId = userId;
-        this.userName = userName;
-        this.token = token;
+    // Getter and Setter methods
+    public String getMessage() {
+        return message;
     }
 
-    // Gettery
-    public String getUserId() {
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getUserId() {
         return userId;
     }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    // Settery
-    public void setUserId(String userId) {
+    public void setUserId(int userId) {
         this.userId = userId;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    // Możesz dodać metodę toString() do łatwego debugowania (opcjonalnie)
-    @Override
-    public String toString() {
-        return "LoginResponse{" +
-                "userId='" + userId + '\'' +
-                ", userName='" + userName + '\'' +
-                ", token='" + token + '\'' +
-                '}';
     }
 }
